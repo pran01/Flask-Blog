1d1fc0cbd9a9239b5d35042fc4ba8b198521ee2b
Pranav Sinha <pran.sinha1.0@gmail.com>
2020-02-26 19:23:07 +0530

    'Update'

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_mail import Mail
+import os
 
 app=Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///blog.db'
@@ -14,8 +15,8 @@ login_manager.login_message_category='info'
 app.config['MAIL_SERVER']='smtp.gmail.com'
 app.config['MAIL_PORT']=587
 app.config['MAIL_USE_TLS']=True
-app.config['MAIL_USERNAME']='pranavsinha.1122000@gmail.com'
-app.config['MAIL_PASSWORD']='De@d1n$1de+gm'
+app.config['MAIL_USERNAME']=os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD']=os.environ.get('MAIL_PASSWORD')
 
 mail=Mail(app)
 
